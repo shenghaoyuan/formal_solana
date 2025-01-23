b5e6c9eda0fd08e026e94133ab5189f0880840b7
Liuhao0917 <2209896142@qq.com>
2025-01-23 17:33:39 +0800

    modify consistency2

--- coq/model/Consistency2.v
+++ coq/model/Consistency2.v
@@ -5,13 +5,14 @@ From bpf.model Require Import ebpf rBPFCommType rBPFSyntax MyTactic.
 From bpf.model Require Import rBPFDecoder rBPFEncoder BitfieldLemma BinSolver.
 
 Lemma rbpf_decode_encode_consistency:
-  forall l_bin pc l ins
-    (HL: list_in_list Int64.eq l_bin pc l = true)
+  forall pc l ins
     (Hdecode: rbpf_decoder pc l = Some ins),
-      rbpf_encoder ins = l_bin.
+     exists l_bin, rbpf_encoder ins = l_bin /\ 
+     list_in_list Int64.eq l_bin pc l = true.
 Proof.
   intros.
   unfold rbpf_encoder, rbpf_decoder in *.
-  destruct ins. 
-  - unfold_bin. simpl.
+  destruct nth_error eqn:Hnth; [| inversion Hdecode].
+  destruct ins; try ( eexists; split; [reflexivity |] ).
+  - unfold_bin. simpl. 
 Admitted.
